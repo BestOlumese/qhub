69ebb60bc64bcf582f0ed8d3bcc316091f070dd9
Amiolemen Best <bestolumese@gmail.com>
2025-09-19 12:23:57 +0100

    Minor fixes

--- components/ui/Form/FormHeading.tsx
+++ components/ui/Form/FormHeading.tsx
@@ -1,10 +1,20 @@
 import React from "react";
-
+import logo from "@/public/logo.png";
+import Image from "next/image";
 const FormHeading = ({ title }: { title: string }) => {
   return (
-    <h1 className="text-black kumbh-sans text-xl sm:text-2xl 2xl:text-4xl font-semibold text-center 2xl:my-2">
-      {title}
-    </h1>
+    <div className="flex flex-col justify-center gap-2">
+      <Image
+        src={logo}
+        alt="logo"
+        priority
+        className="w-20 max-md:w-16" // Increased size
+      />
+      
+      <h1 className="text-black kumbh-sans text-xl sm:text-2xl 2xl:text-4xl font-semibold text-center 2xl:my-2">
+        {title}
+      </h1>
+    </div>
   );
 };
 
